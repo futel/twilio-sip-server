ce977c2b6107eafba07e3430591d77d9831ee712
Karl Anderson <kra@monkey.org>
2023-01-21 14:07:51 -0800

    indentation

--- dialer/functions/sip-outgoing.protected.js
+++ dialer/functions/sip-outgoing.protected.js
@@ -6,7 +6,7 @@
 
 // TODO
 // Validate NANPA.
-// Document requirements.
+// Transform 211 etc.
 // Can we send SNS to our monitoring here? Alternative is Twilio console?
 
 const PNF = require('google-libphonenumber').PhoneNumberFormat;
@@ -39,33 +39,41 @@ exports.handler = function(context, event, callback) {
     console.log(`E.164 To Number: ${toE164Normalized}`);
 
     let mergedAggregatedE164CredentialUsernames = [];
-    credUtil.enumerateCredentialLists(client, sipDomainSid).then(credentialLists => {
-        Promise.all(credentialLists.map(credList => {
-            return credUtil.getSIPCredentialListUsernames(client, credList.sid);
-        })).then(results => {
-            results.forEach(credentials => {
-                // Push all usernames which start with + into mergedAggregatedE164CredentialUsernames.
-                mergedAggregatedE164CredentialUsernames.push
-                    .apply(mergedAggregatedE164CredentialUsernames,
-                           credentials.filter(record => record["username"].startsWith('+'))
-                           .map(record => record.username));
+    credUtil.enumerateCredentialLists(
+        client, sipDomainSid).then(credentialLists => {
+            Promise.all(credentialLists.map(credList => {
+                return credUtil.getSIPCredentialListUsernames(
+                    client, credList.sid);
+            })).then(results => {
+                results.forEach(credentials => {
+                    // Push all usernames which start with + into
+                    // mergedAggregatedE164CredentialUsernames.
+                    mergedAggregatedE164CredentialUsernames.push.apply(
+                        mergedAggregatedE164CredentialUsernames,
+                        credentials.filter(
+                            record => record["username"].startsWith('+'))
+                            .map(record => record.username));
+                });
+                if (mergedAggregatedE164CredentialUsernames.includes(
+                    toE164Normalized)) {
+                    // Our SIP Domain has a credential username which
+                    // matches our To address. Make a SIP URL for that
+                    // username and our SIP domain, and dial it.
+                    console.log('Dialing another E.164 SIP User');
+                    twiml.dial(
+                        {callerId: fromSipCallerId, answerOnBridge: true})
+                        .sip(`sip:${toE164Normalized}@${sipDomain}`);
+                } else {
+                    // We didn't match the To address, dial a PSTN number.
+                    console.log('Dialing a PSTN Number');
+                    twiml.dial(
+                        {callerId: fromSipCallerId, answerOnBridge: true},
+                        toE164Normalized);
+                }
+                callback(null, twiml);
+            }).catch(err => {
+                console.log(err);
+                callback(err);
             });
-            if (mergedAggregatedE164CredentialUsernames.includes(toE164Normalized)) {
-                // Our SIP Domain has a credential username which matches our To address.
-                // Make a SIP URL for that username and our SIP domain, and dial it.
-                console.log('Dialing another E.164 SIP User');
-                twiml.dial({callerId: fromSipCallerId, answerOnBridge: true})
-                    .sip(`sip:${toE164Normalized}@${sipDomain}`);
-            } else {
-                // We didn't match the To address, dial a PSTN number.
-                console.log('Dialing a PSTN Number');
-                twiml.dial({callerId: fromSipCallerId, answerOnBridge: true},
-                           toE164Normalized);
-            }    
-            callback(null, twiml);
-        }).catch(err => {
-            console.log(err);
-            callback(err);
         });
-    });
-};   
+};
