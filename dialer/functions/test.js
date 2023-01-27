3382628df84ab7cfd01edf6dc5babe05e1babe17
Karl Anderson <kra@monkey.org>
2023-01-26 18:55:07 -0800

    rename test

--- dialer/functions/test.js
+++ dialer/functions/test.js
@@ -3,7 +3,7 @@ const assert = require('assert');
 var sinon = require("sinon");
 const util = require('./util.protected.js');
 
-test('foo', (t) => {
+test('getEnvironment', (t) => {
     var context = sinon.fake();
     context.DOMAIN_NAME = "dialer-6443-dev.twil.io";
     environment = util.getEnvironment(context);
