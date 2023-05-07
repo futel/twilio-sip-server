3e266c7d6c9257994d5199d1cf5c088e0927494d
Karl Anderson <kra@monkey.org>
2023-05-06 21:08:02 -0700

    comment

--- dialer/functions/futel-util.protected.js
+++ dialer/functions/futel-util.protected.js
@@ -47,6 +47,7 @@ function getEnvironment(context) {
 
 // Return a URL for the DigitalOcean function for name.
 function getDoFunctionUrl(name, context) {
+    // XXX We only have one namespace, we should return the appropriate stage/prod/dev.
     return "https://" + context.DO_HOST + "/api/v1/web/" + context.DO_NAMESPACE + "/dialplans/" + name
 }
 
