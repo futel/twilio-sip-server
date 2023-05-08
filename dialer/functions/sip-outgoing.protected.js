cd8cb8d3709daf8be58af647fc44bac25888642c
Karl Anderson <kra@monkey.org>
2023-05-08 11:11:06 -0700

    remove unused code

--- dialer/functions/sip-outgoing.protected.js
+++ dialer/functions/sip-outgoing.protected.js
@@ -10,19 +10,10 @@ const extensionMap = JSON.parse(extensionMapAsset.open());
 
 exports.handler = function(context, event, callback) {
     const { From: extensionUri, To: eventToNumber, SipDomainSid: sipDomainSid } = event;
-    const client = context.getTwilioClient();    
     let twiml = new Twilio.twiml.VoiceResponse();
-    let instance = futelUtil.getEnvironment(context);
     
     console.log(`Original from number: ${extensionUri}`);
     console.log(`Original to number: ${eventToNumber}`);
-    // The caller ID is the SIP extension we are calling from, which we assume is E.164.
-    let extension = futelUtil.sipToExtension(extensionUri);
-    let fromNumber = extensionMap[extension].callerId;
-    let enableEmergency = extensionMap[extension].enableEmergency;
-    console.log(`Extension: ${extension}`);    
-    console.log(`SIP CallerID: ${fromNumber}`);
-    console.log(`enableEmergency: ${enableEmergency}`);    
     
     let toNumber = futelUtil.sipToExtension(eventToNumber);
     if (!toNumber) {
@@ -40,34 +31,17 @@ exports.handler = function(context, event, callback) {
         // hopefully we don't care about missing the others.
     }
     console.log(`Normalized to number: ${toNumber}`);
-    toNumber = futelUtil.transformNumber(toNumber);
-    console.log(`Transformed to number: ${toNumber}`);
-    if (futelUtil.filterOutgoingNumber(toNumber)) {
-        console.log("filtered number " + toNumber);
-        twiml.say("We're sorry, your call cannot be completed as dialed. Please check the number and try again.");
-        twiml.reject();
+    if (["#", "0"].includes(toNumber)) {
+        url = new URL(futelUtil.getDoFunctionUrl("dial_sip", context));
+        url.searchParams.append("to_extension", toNumber);
+        url.searchParams.append("from_uri", extensionUri);
+        twiml.redirect(url.href);
         callback(null, twiml);
     } else {
-        if (["#", "0"].includes(toNumber)) {
-            // Send caller to the trunk.
-            let futelExtension = null;
-            if (toNumber == "#") {
-                futelExtension = extensionMap[extension].outgoing;
-            } else if (toNumber == "0") {
-                futelExtension = "operator";
-            }
-            console.log(`trunk extension: ${futelExtension}`);
-            let sipUri = `sip:${futelExtension}@futel-${instance}.phu73l.net;region=us2?x-callerid=${fromNumber}&x-enableemergency=${enableEmergency}`;
-            twiml.dial(
-                {answerOnBridge: true, action: '/sip-outgoing-status'}).sip(
-                    sipUri);
-            callback(null, twiml);
-        } else {
-            url = new URL(futelUtil.getDoFunctionUrl("dial_pstn", context));
-            url.searchParams.append("to_number", toNumber);
-            url.searchParams.append("from_uri", extensionUri);
-            twiml.redirect(url.href);
-            callback(null, twiml);
-        }
+        url = new URL(futelUtil.getDoFunctionUrl("dial_pstn", context));
+        url.searchParams.append("to_uri", eventToNumber);
+        url.searchParams.append("from_uri", extensionUri);
+        twiml.redirect(url.href);
+        callback(null, twiml);
     }
 };
