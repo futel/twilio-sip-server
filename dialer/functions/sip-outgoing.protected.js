5c762f196352451f41fe2bd8b3d90b3ec9205adf
Karl Anderson <kra@monkey.org>
2023-05-09 22:11:54 -0700

    don't filter/reject to number

--- dialer/functions/sip-outgoing.protected.js
+++ dialer/functions/sip-outgoing.protected.js
@@ -5,9 +5,6 @@
 const futelUtilPath = Runtime.getFunctions()['futel-util'].path;
 const futelUtil = require(futelUtilPath);
 
-const extensionMapAsset = Runtime.getAssets()['/extensions.json'];
-const extensionMap = JSON.parse(extensionMapAsset.open());
-
 exports.handler = function(context, event, callback) {
     const { From: extensionUri, To: eventToNumber, SipDomainSid: sipDomainSid } = event;
     let twiml = new Twilio.twiml.VoiceResponse();
@@ -16,21 +13,9 @@ exports.handler = function(context, event, callback) {
     console.log(`Original to number: ${eventToNumber}`);
     
     let toNumber = futelUtil.sipToExtension(eventToNumber);
-    if (!toNumber) {
-        console.log("Could not parse appropriate to number.");
-        twiml.say("We're sorry, your call cannot be completed as dialed. Please check the number and try again.");        
-        twiml.reject();
-        callback(null, twiml);
-        return;
-    }
     
-    try {
-        toNumber = futelUtil.normalizeNumber(toNumber);
-    } catch (error) {
-        // XXX When does the normalizer break? If we couldn't do one normalization step,
-        // hopefully we don't care about missing the others.
-    }
     console.log(`Normalized to number: ${toNumber}`);
+    
     if (["#", "0"].includes(toNumber)) {
         url = new URL(futelUtil.getDoFunctionUrl("dial_sip", context));
         url.searchParams.append("to_extension", toNumber);
