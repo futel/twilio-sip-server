b5080dfa07b3b659082587df00cd7652fc36b9de
Karl Anderson <kra@monkey.org>
2023-05-15 16:23:30 -0700

    mor functionality in do-functions

--- dialer/functions/sip-outgoing.protected.js
+++ dialer/functions/sip-outgoing.protected.js
@@ -1,21 +1,16 @@
 // sip-outgoing: termination for calls originating from a SIP client registered to a Twilio SIP domain.
-// Parse the destination number from the SIP URI.
-// Transform to a PSTN number, validate, filter, dial. Or send to Futel asterisk trunk.
 
 const futelUtilPath = Runtime.getFunctions()['futel-util'].path;
 const futelUtil = require(futelUtilPath);
 
 exports.handler = function(context, event, callback) {
     const { From: extensionUri, To: eventToNumber, SipDomainSid: sipDomainSid } = event;
-    let twiml = new Twilio.twiml.VoiceResponse();
     
-    if (["#", "0"].includes(toNumber)) {
-        url = new URL(futelUtil.getDoFunctionUrl("dial_sip", context));
-    } else {
-        url = new URL(futelUtil.getDoFunctionUrl("dial_pstn", context));
-    }
+    url = new URL(futelUtil.getDoFunctionUrl("dial_outgoing", context));
     url.searchParams.append("to_uri", eventToNumber);
     url.searchParams.append("from_uri", extensionUri);
+
+    let twiml = new Twilio.twiml.VoiceResponse();    
     twiml.redirect(url.href);
     callback(null, twiml);
 };
