ae4eda86b3f6316ad58b48a22a11ef6ab4149105
Karl Anderson <kra@monkey.org>
2023-05-22 21:15:27 -0700

    remove twilio service cruft

--- dialer/functions/futel-util.protected.js
+++
@@ -1,7 +0,0 @@
-// Return a URL for the DigitalOcean function for name.
-function getDoFunctionUrl(name, context) {
-    return "https://" + context.DO_HOST + "/api/v1/web/" + context.DO_NAMESPACE + "/dialers/" + name
-}
-
-exports.getDoFunctionUrl = getDoFunctionUrl;
-
