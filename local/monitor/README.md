2a80ea1e06c7d359be4cb7dd3f0dd5d36fe20318
Karl Anderson <kra@monkey.org>
2024-09-17 15:45:50 -0700

    update test

--- local/monitor/README.md
+++ local/monitor/README.md
@@ -1,6 +1,43 @@
+# Requirements
+
+- openssl
+- ffmpeg
+
 # Setup
 
 To be done once.
+## Create test Application Resource (TwiML app)
+
+Create an Application Resource for testing.
+
+    twilio api:core:applications:create \
+        --voice-method POST \
+        --voice-url "https://twimlets.com/holdmusic?Bucket=com.twilio.music.classical" \
+        --friendly-name "test"
+
+Notes:
+- This is used by local/monitor.
+- This is nonoptimal, what we really want is a SIP client that won't hang up or interact with our dialplans.
+
+## Create test phone number
+
+Use the web GUI; the APIs may allow us to do this, but maybe not.
+
+Create new phone number
+- friendly name: test
+- emergency calling: not registered
+- voice configuration:
+    - configure with: TwiML App
+    - TwiML App: test
+- messaging configuation:
+    - a message comes in: webhook
+    - URL: blank
+
+Notes:
+- This is used by local/monitor.
+- This is nonoptimal, what we really want is a SIP client that won't hang up or interact with our dialplans.
+
+Note that this is nonoptimal, what we really want is a SIP client that won't hang up or interact with our dialplan.
 
 ## Set up environment secrets
 
@@ -15,4 +52,11 @@ Fill .env to match .env.sample.
 # Run
 
 - source venv/bin/activate
-- python3 call_logs.py
+- python3 outbound.py
+
+# Notes
+
+Would be better to have a SIP client to accept a call and do nothing until caller hangs up.
+
+- https://github.com/AGProjects/python3-sipsimple
+- https://github.com/sippy/b2bua?tab=readme-ov-file
--- local/monitor/outbound.py
+++ local/monitor/outbound.py
@@ -1,9 +1,6 @@
 """
-Initiate calls to our dialplan for testing.
+Use the twilio api to call our extensions regularly.
 """
-
-# Could we use this to monitor registration? like traceroute, increase
-# timeout until we get something informative when calling a SIP endpoint?
 
 import dotenv
 import os
@@ -16,30 +13,14 @@ account_sid = os.environ["TWILIO_ACCOUNT_SID"]
 auth_token = os.environ["TWILIO_AUTH_TOKEN"]
 client = Client(account_sid, auth_token)
 
-#to = "sip:test-one@direct-futel-stage.sip.twilio.com"
-#to = "sip:dome-office@direct-futel-prod.sip.twilio.com"
-# A test number set up in our deployment. This is a stupid way to test because
-# we have to pay for the number and the SIP leg.
-# What we want is a SIP URL for a client that will answer and not hang up.
-to = "+19713512383"
-# Context to test.
+extension = "dome-office"
+to = 'sip:{}@direct-futel-prod.sip.twilio.com'.format(extension)
+# Context for callee to experience.
 context = "outgoing_detroit"
-# URL to return twiml. This is what we are testing, the total length of the call
-# over all requests. We could also send twiml to test specific content.
+# URL to return twiml for dialplan.
 url = "https://prod.dialplans.phu73l.net/ivr?context={}".format(context)
 
 call = client.calls.create(
     to=to,
-    from_="+15034681337",       # Arbitrary.
+    from_="+15034681337",
     url=url)
-
-print(call.sid)
-# We could instead set up an event callback to find out when the call ends,
-# maybe an ops url that also logs duration?
-while True:
-    call_out = client.calls(call.sid).fetch()
-    print(call_out.status)
-    if call_out.status not in ['queued', 'in-progress']:
-        break
-    time.sleep(5)
-print(call_out.duration)
